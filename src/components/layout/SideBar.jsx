7d7f76f2004d3cb83e98744b5b9e40030d125447
Aash <31497659+AashPau@users.noreply.github.com>
2024-07-13 11:15:45 +1000

    add navigation for admin layout sidebar

--- src/components/layout/SideBar.jsx
+++ src/components/layout/SideBar.jsx
@@ -1,16 +1,45 @@
 import Stack from "react-bootstrap/Stack";
+import { Link } from "react-router-dom";
 
 export const SideBar = () => {
   return (
-    <Stack gap={3}>
-      <div className="p-2">Dashboard</div>
-      <div className="p-2">Categories</div>
-      <div className="p-2">Products</div>
-      <div className="p-2">Users</div>
-      <div className="p-2">Orders</div>
-      <div className="p-2">Reviews</div>
+    <Stack gap={1}>
+      <div className="p-2">
+        <Link className="nav-link" to="/admin/dashboard">
+          Dashboard
+        </Link>
+      </div>
+      <div className="p-2">
+        <Link className="nav-link" to="/admin/categories">
+          Categories
+        </Link>
+      </div>
+      <div className="p-2">
+        <Link className="nav-link" to="/admin/products">
+          Products
+        </Link>
+      </div>
+      <div className="p-2">
+        <Link className="nav-link" to="/admin/users">
+          Users
+        </Link>
+      </div>
+      <div className="p-2">
+        <Link className="nav-link" to="/admin/orders">
+          Orders
+        </Link>
+      </div>
+      <div className="p-2">
+        <Link className="nav-link" to="/admin/reviews">
+          Reviews
+        </Link>
+      </div>
       <hr />
-      <div className="p-2">Admins</div>
+      <div className="p-2">
+        <Link className="nav-link" to="/admin/admins">
+          Admins
+        </Link>
+      </div>
     </Stack>
   );
 };

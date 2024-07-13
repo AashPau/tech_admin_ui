fd83b3508cdf497381e905aa7fd6a07dd36801c1
Aash <31497659+AashPau@users.noreply.github.com>
2024-07-13 11:17:55 +1000

    add log out function

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,20 +2,27 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { useDispatch } from "react-redux";
+import { FaUser } from "react-icons/fa";
+import { logoutUserAction } from "../../features/user/userAction";
 
 export const Header = () => {
+  const dispatch = useDispatch();
   return (
     <Navbar expand="md" className="bg-dark " data-bs-theme="dark">
       <Container>
-        <Navbar.Brand href="#home"></Navbar.Brand>
+        <Navbar.Brand href="#home"> </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <NavDropdown title={<FaUser />} id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.1">Log Out</NavDropdown.Item>
+              <NavDropdown.Item
+                onClick={() => dispatch(logoutUserAction())}
+                href="#"
+              >
+                Logout
+              </NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
